e162830af925b9f84d4e270105cdfdb6baa4eb4c
zhaoyangming <zhaoyangming@deepexi.com>
2023-09-19 10:19:57 +0800

    jdbc hive dialect

--- externals/kyuubi-jdbc-engine/src/main/scala/org/apache/kyuubi/engine/jdbc/hive/GetSchemas.scala
+++ externals/kyuubi-jdbc-engine/src/main/scala/org/apache/kyuubi/engine/jdbc/hive/GetSchemas.scala
@@ -34,6 +34,8 @@ class GetSchemas(session: Session)
         0L,
         false)
       hiveStatement.executeStatement()
+      schema = hiveStatement.schema
+      iter = hiveStatement.iter
     } catch onError()
   }
 }
